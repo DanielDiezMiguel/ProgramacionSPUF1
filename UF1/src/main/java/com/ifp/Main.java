2bce71a6a431109782c660c0e71fa3dadb123de5
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-30 12:29:18 +0200

    DES terminado

--- UF1/src/main/java/com/ifp/Main.java
+++ UF1/src/main/java/com/ifp/Main.java
@@ -1,37 +1,14 @@
 package com.ifp;
 
-import com.ifp.cifradoAES.CifradoAES;
 import com.ifp.cifradoDES.CifradoDES;
 
-import javax.crypto.*;
-import java.io.IOException;
-import java.security.InvalidKeyException;
+import javax.crypto.NoSuchPaddingException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Scanner;
-
-import static java.lang.System.*;
-import static javax.crypto.Cipher.*;
-import static javax.crypto.Cipher.DECRYPT_MODE;
-import static javax.crypto.Cipher.ENCRYPT_MODE;
 
 public class Main {
-    public static void main(String[] args) throws IOException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
-        Scanner scanner = new Scanner(in);
-        int respuesta = 0;
+    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException {
 
-        while (respuesta != 3) {
-            out.println("|=============================|" + lineSeparator() +
-                    "|1) Cifrado AES               |" + lineSeparator() +
-                    "|2) Cifrado DES               |" + lineSeparator() +
-                    "|3) Salir                     |" + lineSeparator() +
-                    "|=============================|");
-            out.println("Introduce una respuesta: ");
-            respuesta = scanner.nextInt();
-
-            switch (respuesta) {
-                case 1 -> CifradoAES.cifradoAES();
-                case 2 -> CifradoDES.cifradoDES();
-            }
-        }
+        CifradoDES.cifradoDES();
+        
     }
 }
